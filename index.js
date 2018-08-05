28de9ff9f631b73e6ea4ec2941acaf9e66b39b9d
Steven Beyer <sbeyerjr@gmail.com>
2018-08-05 15:32:26 -0700

    more changes to html

--- index.js
+++ index.js
@@ -120,8 +120,8 @@ function generateQuestionBlock() {
     <input type="radio" name="answer" id ="option2" value="${STORE[i].options[1]}"><label for="option2">${STORE[i].options[1]}</label><br>
 	<input type="radio" name="answer" id ="option3" value="${STORE[i].options[2]}"><label for="option3">${STORE[i].options[2]}</label><br>
 	<input type="radio" name="answer" id ="option4" value="${STORE[i].options[3]}"><label for="option4">${STORE[i].options[3]}</label><br>
-	<div class="btn-sub"><button type="submit" id="button-submit" value="Go">Submit Answer</button></div>
 	</fieldset>
+	<div class="btn-sub"><button type="submit" id="button-submit" value="Go">Submit Answer</button></div>
    </form></div>
    
 `;
